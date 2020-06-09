f17ef1fbe37321451e943581ee2902df8ac0e477
takumi-nakajima <takumi.nakajima@gmail.com>
2020-06-09 16:43:28 +0900

    コメントモデルの追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,6 +79,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+#    'default': {
+#        #'ENGINE': 'django.db.backends.postgresql',
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'djangogirls',
+#        'USER': 'takumi',
+#        'PASSWORD': 'nakajima3',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
 }
 
 
@@ -122,3 +131,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
